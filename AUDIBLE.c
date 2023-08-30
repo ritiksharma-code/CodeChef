bfed611c3d77319e130d4e5c7a3d37027379072a
ritiksharma-code <ritiksharma0405@gmail.com>
2023-08-31 01:32:50 +0530

    AUDIBLE

---
+++ AUDIBLE.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x;
+	scanf("%d", &t);
+	while(t--)
+	{
+	    scanf("%d", &x);
+	    if(x>=67&&x<=45000)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
