3a6dd1f8357e527c610eb05cd42f93c414864100
Ritik Sharma <ritiksharma0405@gmail.com>
2023-10-15 15:31:43 +0530

    15-10-2023

---
+++ NEWSPAPER.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x>=8 && x<=10)
+	{
+	    cout<<"YES\n";
+	}
+	else
+	{
+	    cout<<"NO\n";
+	}
+	return 0;
+}
